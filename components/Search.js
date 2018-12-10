7777a40c8cb26ff462cba7fdf94fa8bd13257b32
Thibault <thibault@MacBook-Pro-de-Thibault.local>
2018-12-10 12:56:39 +0100

    2eme commit

---
+++ components/Search.js
@@ -0,0 +1,39 @@
+// Components/Search.js
+
+import React from 'react'
+import {StyleSheet, View, TextInput, Button, FlatList, Text} from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
+
+class Search extends React.Component {
+  render(){
+    return (
+      <View style={styles.main_container}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
+        <Button style={{ height: 50 }} title='Rechercher' onPress={() => {}}/>
+        <FlatList
+          data={films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor : '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
