f98e0cf823bacb4f088570e564ea9112acf417e6
Thibault Barrat <thibault.barrat@harmonie-mutuelle.fr>
2018-12-12 15:01:58 +0100

    wip 2018-12-12

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -1,13 +1,54 @@
 // components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBAPI'
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+
+  compoenentDidMount() {
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+  }
+
+  _displayLoading() {
+    if(this.state.isLoading) {
+      return(
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
+  }
+
+  _displayFilm() {
+    if(this.state.film != undefined) {
+      return(
+        <ScrollView style={styles.scrollview_container}>
+          <Image
+            style={styles.image}
+            source={{uri}}
+        </ScrollView>
+      )
+    }
+  }
+
   render() {
-    return (
+    return(
       <View style={styles.main_container}>
-        <Text>Détail du film {this.props.navigation.state.params.idFilm}</Text>
+        {this._displayLoading()}
+        {this._displayFilm()}
       </View>
     )
   }
@@ -16,6 +57,18 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  scrollview_container: {
+    flex: 1
   }
 })
 
