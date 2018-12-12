e8ee69ad4975859468d48399f5016a059419c842
Thibault <thibault@MacBook-Pro-de-Thibault.local>
2018-12-12 23:38:25 +0100

    wip 2018-12-12-2

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -1,8 +1,10 @@
 // components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
 import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBAPI'
+import moment from 'moment'
+import numeral from 'numeral'
 
 class FilmDetail extends React.Component {
   constructor(props) {
@@ -13,7 +15,7 @@ class FilmDetail extends React.Component {
     }
   }
 
-  compoenentDidMount() {
+  componentDidMount() {
     getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
       this.setState({
         film: data,
@@ -33,12 +35,28 @@ class FilmDetail extends React.Component {
   }
 
   _displayFilm() {
-    if(this.state.film != undefined) {
+    const { film } = this.state
+    if(film != undefined) {
       return(
         <ScrollView style={styles.scrollview_container}>
           <Image
             style={styles.image}
-            source={{uri}}
+            source={{uri: getImageFromApi(film.backdrop_path)}}
+          />
+          <Text style={styles.title_text}>{film.title}</Text>
+          <Text style={styles.description_text}>{film.overview}</Text>
+          <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
+          <Text style={styles.default_text}>Note : {film.vote_average}/10</Text>
+          <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
+          <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+          <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
+              return genre.name;
+            }).join(" / ")}
+          </Text>
+          <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
+              return company.name;
+            }).join(" / ")}
+          </Text>
         </ScrollView>
       )
     }
@@ -69,6 +87,33 @@ const styles = StyleSheet.create({
   },
   scrollview_container: {
     flex: 1
+  },
+  image: {
+    height: 169,
+    margin: 5
+  },
+  title_text: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    flex: 1,
+    flexWrap: 'wrap',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#000000',
+    textAlign: 'center'
+  },
+  description_text: {
+    fontStyle: 'italic',
+    color: '#666666',
+    margin: 5,
+    marginBottom: 15
+  },
+  default_text: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
   }
 })
 
